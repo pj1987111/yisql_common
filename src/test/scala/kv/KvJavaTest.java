ae10be7aed80b3dd18b1d5a7da7a7b0b9a62c264
hongyi.zhou <hongyi.zhou@@tongdun.cn>
2021-05-02 16:10:01 +0800

    add support arrow

--- src/test/scala/kv/KvJavaTest.java
+++ src/test/scala/kv/KvJavaTest.java
@@ -1,9 +1,7 @@
 package kv;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
+import com.zhy.yisql.common.utils.kv.InMemoryStore;
 import com.zhy.yisql.common.utils.kv.KVIndex;
-import com.zhy.yisql.common.utils.kv.KVStoreSerializer;
 import com.zhy.yisql.common.utils.kv.LevelDB;
 import org.junit.Test;
 
@@ -20,53 +18,42 @@ import java.io.Serializable;
  */
 public class KvJavaTest {
   @Test
-  public void simple() throws Exception {
+  public void leveldbTest() throws Exception {
     LevelDB lDb = new LevelDB(new File("java_example"));
     lDb.write(new JCustomClassItem("testval", "456"));
     JCustomClassItem wapper = lDb.read(JCustomClassItem.class, "testval");
     System.out.println(wapper);
   }
+
+  @Test
+  public void inMemoTest() throws Exception {
+    InMemoryStore inMemoryStore = new InMemoryStore();
+    inMemoryStore.write(new JCustomClassItem("testval1", "456"));
+    inMemoryStore.write(new JCustomClassItem("testval2", "456"));
+    JCustomClassItem wapper1 = inMemoryStore.read(JCustomClassItem.class, "testval1");
+    JCustomClassItem wapper2 = inMemoryStore.read(JCustomClassItem.class, "testval2");
+    System.out.println(1);
+  }
+
+  static class JCustomClassItem implements Serializable {
+    @KVIndex
+    String name;
+    String value;
+
+    JCustomClassItem() {
+    }
+
+    JCustomClassItem(String name, String value) {
+      this.name = name;
+      this.value = value;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public String getValue() {
+      return value;
+    }
+  }
 }
-
-class JCustomClassItem implements Serializable {
-  @KVIndex
-  String name;
-  String className;
-
-  JCustomClassItem() {
-  }
-
-  JCustomClassItem(String name, String className) {
-    this.name = name;
-    this.className = className;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getClassName() {
-    return className;
-  }
-}
-
-//class JCustomClassItemWrapper implements Serializable {
-//  @JsonIgnore
-//  @KVIndex
-//  String id() {
-//    return customClassItem.name;
-//  }
-//
-//  JCustomClassItem customClassItem;
-//
-//  JCustomClassItemWrapper() {
-//  }
-//
-//  JCustomClassItemWrapper(JCustomClassItem customClassItem) {
-//    this.customClassItem = customClassItem;
-//  }
-//
-//  public JCustomClassItem getCustomClassItem() {
-//    return customClassItem;
-//  }
-//}
